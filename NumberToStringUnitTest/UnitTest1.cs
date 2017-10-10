0a630e1222f942ba9fa96c5bb0f424a5211cb404
kianoush <kianoush.karbasi@gmail.com>
2017-10-10 11:39:47 +1100

    Added a test project

---
+++ NumberToStringUnitTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumberToString.Controllers;
+using NumberToString.Services;
+
+namespace NumberToStringUnitTest
+{
+    [TestClass]
+    public class ConvertControllerShould
+    {
+        [TestMethod]
+        public void ParseIntsCorrectly()
+        {
+            var expectedName = "test";
+
+            var convertingService = new ConvertingService();
+            var result = convertingService.ToEnglish(expectedName, 1);
+
+            Assert.AreEqual(expectedName ,result.Name);
+            Assert.AreEqual("ONE", result.Amount);
+        }
+    }
+}
